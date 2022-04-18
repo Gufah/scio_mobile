50729e407b00998018c23c50b8a0e2a14fbe6a5c
Ugonna Fredrick <ugonna.fredrick@accenture.com>
2022-04-18 16:46:12 +0200

    change the first screen

--- src/screens/Welcome.tsx
+++ src/screens/Welcome.tsx
@@ -1,44 +1,35 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  View,
-  Text,
-  StyleSheet,
-  ImageBackground,
-  StatusBar,
-} from 'react-native';
-import welcomeScreenImg from '../../images/money-wallpaper-ixpap-9.jpeg';
+import { SafeAreaView, View, StyleSheet, StatusBar, Image } from 'react-native';
 import { Button } from '../components/Button';
 import colors from '../components/styles/Colors';
 import { useNavigation } from '@react-navigation/native';
+import digimod from '../../images/digi-mod.png';
 
 export const WelcomeScreen = () => {
   const { navigate } = useNavigation();
   return (
-    <ImageBackground source={welcomeScreenImg} style={styles.background}>
+    <SafeAreaView style={styles.container}>
       <StatusBar barStyle='dark-content' />
-      <SafeAreaView style={styles.container}>
-        <View style={styles.title}>
-          <Text style={styles.titleText}>SCIO</Text>
+      <View style={styles.title}>
+        <Image source={digimod} />
+      </View>
+      <View style={styles.buttonDiv}>
+        <View style={styles.buttons}>
+          <Button
+            title='Login'
+            onPress={() => navigate('Log In')}
+            textStyle={styles.textStyle}
+          />
+          <Button
+            title='Register'
+            onPress={() => {
+              return;
+            }}
+            textStyle={styles.textStyle}
+          />
         </View>
-        <View style={styles.buttonDiv}>
-          <View style={styles.buttons}>
-            <Button
-              title='Login'
-              onPress={() => navigate('Log In')}
-              textStyle={styles.textStyle}
-            />
-            <Button
-              title='Register'
-              onPress={() => {
-                return;
-              }}
-              textStyle={styles.textStyle}
-            />
-          </View>
-        </View>
-      </SafeAreaView>
-    </ImageBackground>
+      </View>
+    </SafeAreaView>
   );
 };
 
@@ -46,22 +37,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginBottom: 30,
-  },
-  background: {
-    flex: 1,
+    backgroundColor: colors.white,
   },
   title: {
     flex: 1,
     flexDirection: 'column',
     justifyContent: 'center',
-    textAlign: 'center',
-    marginHorizontal: 20,
-  },
-  titleText: {
-    color: colors.black,
-    fontSize: 40,
-    textAlign: 'center',
-    fontWeight: 'bold',
+    alignItems: 'center',
+    alignSelf: 'center',
   },
   buttonDiv: {
     alignItems: 'center',
